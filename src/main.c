b754d1321ebd734d8c2c3ae5c2c5a3ed81f8c521
Agape Maria <agape.maria03@gmail.com>
2025-04-21 21:59:37 +0300

    Partially successful message transmission/reception

--- src/main.c
+++ src/main.c
@@ -2,23 +2,29 @@
 // TX
 //*********************************************/
 
-#include "C:\Users\agape\Documents\LICENTA\functions\includes.h"
+#include "C:\Users\agape\Documents\LICENTA\dw1000_app\DW1000-driver\includes.h"
 
 int main(void)
 {
-    if(check_devices_ready()){
+    if (check_devices_ready())
+    {
         LOG_ERR("Devices not ready!");
         return 1;
     }
-    gpio_pin_configure_dt(&reset_gpio, GPIO_OPEN_DRAIN|GPIO_OUTPUT);
+    gpio_pin_configure_dt(&reset_gpio, GPIO_OPEN_DRAIN | GPIO_OUTPUT);
     reset_devices();
+
+    // soft reset
+    // uint8_t softreset = 0x00;
+    // dw1000_subwrite_u8(PMSC, 0x04, softreset);
+
     LOG_INF("\n[TX]");
     uint32_t dev_id;
     dw1000_read_u32(0x00, &dev_id);
-    initialize();
-    // dw1000_default_config();
+    // initialize();
+    dw1000_default_config();
 
-    //configure();
+    // configure();
 
     while (1)
     {
@@ -29,12 +35,23 @@ int main(void)
         dw1000_read_u32(0x00, &dev_id);
 
         // TX_BUFFER = 0x09
-        uint32_t tx_data = 0xACACACAC;             
-        dw1000_write_u32(0x09, tx_data); 
-        
-        uint32_t reg32 = pdw1000local.txFCTRL | sizeof(tx_data) | (0 << 22) | (0 << 15);
-        dw1000_write_u32(TX_FCTRL, reg32);
-        
+        uint32_t tx_data = 0xACACCCAC;
+        dw1000_write_u32(0x09, tx_data);
+
+        // uint32_t reg32 = pdw1000local.txFCTRL | sizeof(tx_data) | (0 << 22) | (0 << 15);
+
+        uint32_t tx_fctrl = 0;
+        tx_fctrl |= (6 & 0x7F); // TFLEN
+        tx_fctrl |= (0 << 7);   // TFLE = 0
+        tx_fctrl |= (0 << 10);  // Reserved = 0
+        tx_fctrl |= (2 << 13);  // TXBR = 10 (6.8 Mbps)
+        tx_fctrl |= (1 << 16);  // TXPRF = 01 (16 MHz)
+        tx_fctrl |= (1 << 18);  // TXPSR = 01
+        tx_fctrl |= (1 << 20);  // PE = 01
+        tx_fctrl |= (0 << 22);  // TXBOFFS = 0x000
+
+        dw1000_write_u32(TX_FCTRL, tx_fctrl);
+
         tx_start();
         // SYSTEM EVENT = 0x0F -> wait for transmission completion
         uint32_t sys_status;
@@ -51,6 +68,6 @@ int main(void)
 
         k_msleep(SLEEP_TIME_MS);
     }
- 
+
     return 0;
 }
