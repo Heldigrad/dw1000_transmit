21a8f73b3ffc4b51c31b8b789aadf82b69b21b34
Agape Maria <agape.maria03@gmail.com>
2025-04-26 15:04:45 +0300

    Started single-sided two way ranging - determined T1

--- src/main.c
+++ src/main.c
@@ -14,52 +14,24 @@ int main(void)
     gpio_pin_configure_dt(&reset_gpio, GPIO_OPEN_DRAIN | GPIO_OUTPUT);
     reset_devices();
 
-    // soft reset
-    // uint8_t softreset = 0x00;
-    // dw1000_subwrite_u8(PMSC, 0x04, softreset);
-
-    LOG_INF("\n[TX]");
-    uint32_t dev_id;
-    dw1000_read_u32(0x00, &dev_id);
-
-    // generic_default_configs();
-    // tx_default_configs();
-    // additional_default_configs();
-
-    // initialize();
-    // configure();
-
     while (1)
     {
-        LOG_INF("SIMPLE_TX");
+        LOG_INF("\n\n");
+        LOG_INF("TX");
+
+        // Device ID
+        // uint32_t dev_id;
+        // dw1000_read_u32(0x00, &dev_id);
 
         reset_devices();
 
-        generic_default_configs();
+        generic_default_configs(4);
         tx_default_configs();
         additional_default_configs();
 
-        // Device ID
-        uint32_t dev_id;
-        dw1000_read_u32(0x00, &dev_id);
-
         // TX_BUFFER = 0x09
-        uint32_t tx_data = 0xAABAAB;
-        dw1000_write_u32(0x09, tx_data);
-
-        // uint32_t reg32 = pdw1000local.txFCTRL | sizeof(tx_data) | (0 << 22) | (0 << 15);
-
-        uint32_t tx_fctrl = 0;
-        tx_fctrl |= (6 & 0x7F); // TFLEN
-        tx_fctrl |= (0 << 7);   // TFLE = 0
-        tx_fctrl |= (0 << 10);  // Reserved = 0
-        tx_fctrl |= (2 << 13);  // TXBR = 10 (6.8 Mbps)
-        tx_fctrl |= (1 << 16);  // TXPRF = 01 (16 MHz)
-        tx_fctrl |= (1 << 18);  // TXPSR = 01
-        tx_fctrl |= (1 << 20);  // PE = 01
-        tx_fctrl |= (0 << 22);  // TXBOFFS = 0x000
-
-        // dw1000_write_u32(TX_FCTRL, tx_fctrl);
+        uint32_t tx_data = POLL_MSG; // Poll message - to initiate ranging
+        dw1000_write_u32(TX_BUFFER, tx_data);
 
         tx_start();
         // SYSTEM EVENT = 0x0F -> wait for transmission completion
@@ -70,10 +42,45 @@ int main(void)
             k_msleep(10);
         } while (!(sys_status & SYS_STATUS_TXFRS)); // Check TXFRS bit
 
-        LOG_INF("Transmission complete! Clearing bits in SYS_STATUS.");
+        uint64_t T1 = get_tx_timestamp();
+        LOG_INF("TX success! T1 = %llX", T1);
 
         // SYSTEM EVENT = 0x0F -> clear TXFRS flag
         dw1000_write_u32(SYS_STATUS, SYS_STATUS_TXFRS);
+
+        // RECEIVE response
+        // generic_default_configs(4);
+        // rx_default_configs();
+        // // additional_default_configs();
+
+        // rx_enable();
+
+        // // Wait for a valid frame (RXFCG bit in SYS_STATUS)
+        // uint32_t sys_status_1 = {0};
+        // do
+        // {
+        //     dw1000_read_u32(SYS_STATUS, &sys_status_1);
+        //     k_msleep(100);
+        // } while (!(sys_status_1 & (SYS_STATUS_RXFCG | SYS_STATUS_ALL_RX_ERR))); // Check ERR bits | RXFCG bit
+
+        // if (sys_status_1 & SYS_STATUS_RXFCG)
+        // {
+        //     // Read received data
+        //     uint64_t T4 = get_rx_timestamp();
+        //     LOG_INF("Reception success! T4 = %X", T4);
+        //     uint32_t T2;
+        //     dw1000_read_u32(RX_BUFFER, &T2);
+
+        //     // Clear status bit
+        //     // LOG_INF("Clearing status bit...");
+        //     dw1000_write_u32(SYS_STATUS, SYS_STATUS_RX_OK);
+        // }
+        // else
+        // {
+        //     // Clear err bits
+        //     LOG_INF("Reception failed. Resend message! Clearing err bits...");
+        //     dw1000_write_u32(SYS_STATUS, SYS_STATUS_ALL_RX_ERR);
+        // }
 
         k_msleep(SLEEP_TIME_MS);
     }
