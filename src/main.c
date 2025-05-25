0f1cedfb25bb3201e7d25e02cb0d7982710c9a78
heldigrad <agape.maria03@gmail.com>
2025-05-25 20:30:42 +0300

    Fix loading of leading edge detection microcode

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 // TX
 //*********************************************/
 
-#include "C:\Users\agape\Documents\LICENTA\dw1000_app\DW1000-driver\includes.h"
+#include "C:\Users\agape\Documents\LICENTA\functions\includes.h"
 
 int main(void)
 {
@@ -14,7 +14,14 @@ int main(void)
     gpio_pin_configure_dt(&reset_gpio, GPIO_OPEN_DRAIN | GPIO_OUTPUT);
     reset_devices();
 
-    uint32_t tx_data = 0x12345678;
+    reset_devices();
+
+    new_init();
+    new_configure();
+
+    dw1000_write_u32(SYS_STATUS, 0xFFFFFFFF);
+
+    uint32_t tx_data = 0xB00B135;
     uint32_t status;
 
     while (1)
@@ -24,29 +31,24 @@ int main(void)
 
         // Device ID
         uint32_t dev_id;
-
-        reset_devices();
-
-        // dw1000_read_u32(0x00, &dev_id);
-
-        stolen_init();
-        stolen_configure();
-
         // TX_BUFFER = 0x09
         // Write the data to the IC TX buffer, (-2 bytes for auto generated CRC)
         dw1000_write_u32(TX_BUFFER, tx_data);
 
-        stolen_set_txfctrl(6, 0, 0);
+        new_set_txfctrl(6, 0, 0);
 
-        stolen_tx_start(0);
+        new_tx_start(0);
 
         dw1000_read_u32(SYS_STATUS, &status);
         while (!(status & SYS_STATUS_TXFRS))
         {
             dw1000_read_u32(SYS_STATUS, &status);
         }
+
+        print_enabled_bits(status);
+
         uint64_t T1 = get_tx_timestamp();
-        LOG_INF("TX success! T1 = %08llX", T1);
+        LOG_INF("TX success! T1 = %09llX", T1);
 
         /* Clear TX frame sent event. */
         dw1000_write_u32(SYS_STATUS, SYS_STATUS_TXFRS);
