daede0a5dd42ccbdb78a33aaa014486acaa2f49a
heldigrad <agape.maria03@gmail.com>
2025-06-01 17:09:52 +0300

    TWR Attempt

--- src/main.c
+++ src/main.c
@@ -20,60 +20,59 @@ int main(void)
     bip_init();
     bip_config();
 
+    dw1000_subwrite_u16(0x0C, 0x00, 0x4C33); // rx timeout
+
+    set_rx_antenna_delay(RX_ANT_DLY);
+    set_tx_antenna_delay(TX_ANT_DLY);
+
     uint64_t T1, T2, T3, T4, aux;
-    uint64_t Tround, Treply, Tprop;
-    const double SPEED_OF_LIGHT = 299702547.0;
-    double distance, tof;
-    int ok = 1;
+    double distance;
+    int cnt = 0;
     dw1000_write_u32(SYS_STATUS, 0xFFFFFFFF);
 
-    dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
-    dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
+    double mean = 0;
 
-    // while (1)
+    for (int i = 0; i < 10; ++i)
     {
         LOG_INF("\n\n");
 
+        dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
+        dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
+
         if (transmit(POLL_MSG, 5, &T1) == SUCCESS)
         {
-            LOG_INF("TX Success! Waiting for timestamp nr. 2");
+            // LOG_INF("TX Success! Waiting for timestamp nr. 2");
             if (receive(&T2, &T4) == SUCCESS)
             {
-                LOG_INF("TX Success! Waiting for timestamp nr. 3");
+                // LOG_INF("TX Success! Waiting for timestamp nr. 3");
                 if (receive(&T3, &aux) == SUCCESS)
                 {
-                    LOG_INF("All required timestamps were acquired!");
+                    // LOG_INF("All required timestamps were acquired!");
                 }
                 else
                 {
-                    ok = 0;
+                    continue;
                 }
             }
             else
             {
-                ok = 0;
+                continue;
             }
         }
         else
         {
-            ok = 0;
+            continue;
         }
 
-        if (ok == 0)
-        {
-            LOG_INF("Something went wrong. Please try again!");
-        }
-        else
-        {
-            Tround = T4 - T1;
-            Tprop = (Tround - Treply) / 2;
-            tof = (double)Tprop * DWT_TIME_UNITS;
-            distance = Tprop * SPEED_OF_LIGHT;
-            LOG_INF("T1 = %llX, T2 = %llX, T3 = %llX, T4 = %llX, Distance = %llX", T1, T2, T3, T4, distance);
-        }
-
-        k_msleep(RX_SLEEP_TIME_MS);
+        distance = compute_distance(T1, T2, T3, T4);
+        LOG_INF("T1 = %llX, T2 = %llX, T3 = %llX, T4 = %llX, Distance = %f m", T1, T2, T3, T4, distance);
+        mean += distance;
+        cnt++;
+        // k_msleep(RX_SLEEP_TIME_MS);
     }
+
+    mean /= cnt;
+    LOG_INF("Mean distance = %f m", mean);
 
     return 0;
 }
