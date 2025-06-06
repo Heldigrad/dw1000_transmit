b7ca80764cc0d17a8847a401d5b43504694b5720
heldigrad <agape.maria03@gmail.com>
2025-06-06 15:38:23 +0300

    Functional SSTWR

--- src/main.c
+++ src/main.c
@@ -20,53 +20,54 @@ int main(void)
     bip_init();
     bip_config();
 
-    dw1000_subwrite_u16(0x0C, 0x00, 0x4C33); // rx timeout
+    // dw1000_subwrite_u16(0x0C, 0x00, 0x4C33); // rx timeout
 
     set_rx_antenna_delay(RX_ANT_DLY);
     set_tx_antenna_delay(TX_ANT_DLY);
 
-    set_rx_after_tx_delay(POLL_TX_TO_RESP_RX_DLY_UUS);
-    // set_rx_timeout(RESP_RX_TIMEOUT_UUS * 500000);
+    // set_rx_after_tx_delay(POLL_TX_TO_RESP_RX_DLY_UUS);
+    //  set_rx_timeout(RESP_RX_TIMEOUT_UUS * 500000);
 
-    uint64_t T1, T2, T3, T4, aux;
-    double values[20];
-    double distance, sum;
-    int cnt = 0;
+    uint64_t T1, T4, aux;
     dw1000_write_u32(SYS_STATUS, 0xFFFFFFFF);
 
     double mean = 0;
 
-    for (int i = 0; i < 10; ++i)
+    while (1)
     {
         T1 = 0;
-        T2 = 0;
-        T3 = 0;
         T4 = 0;
         dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
         dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
 
         transmit(POLL_MSG, 5, &T1);
-        receive(&T2, &T4);
-        transmit(0xA987654321, 5, &aux); // confirmation
-        receive(&T3, &aux);
-
-        distance = compute_distance(T1, T2, T3, T4);
-        if (distance > 100)
+        receive(&aux, &T4);
+        if (aux == 0x1020304050)
         {
-            continue;
+            dw1000_subwrite_u40(TX_BUFFER, 0x00, T1);
+            dw1000_subwrite_u40(TX_BUFFER, 0x05, T4);
+
+            new_set_txfctrl(10);
+
+            new_tx_start(0);
+
+            uint32_t status;
+            do
+            {
+                dw1000_read_u32(SYS_STATUS, &status);
+            } while (!(status & SYS_STATUS_TXFRS | SYS_STATUS_ALL_TX_ERR));
+
+            if (!(status & SYS_STATUS_ALL_TX_ERR))
+            {
+                /* Clear TX frame sent event. */
+                dw1000_write_u32(SYS_STATUS, SYS_STATUS_TX_OK);
+            }
+            else
+            {
+                /* Clear TX error event. */
+                dw1000_write_u32(SYS_STATUS, SYS_STATUS_TX_OK | SYS_STATUS_ALL_TX_ERR);
+            }
         }
-        LOG_INF("T1 = %llX, T2 = %llX, T3 = %llX, T4 = %llX, Distance = %f m", T1, T2, T3, T4, distance);
-        sum += distance;
-        cnt++;
-        // k_msleep(RX_SLEEP_TIME_MS);
-
-        // LOG_INF("T1 = %llX, T2 = %llX, T3 = %llX, T4 = %llX", T1, T2, T3, T4);
+        LOG_INF("T1 = %llX, T4 = %llX", T1, T4);
     }
-
-    if (sum > 0)
-    {
-        sum /= cnt;
-        LOG_INF("Raw mean = %f", sum);
-    }
-    return 0;
 }
