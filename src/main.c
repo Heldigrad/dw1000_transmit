3895bfe2dc2ee10857d0b121617b85e968289ae2
heldigrad <agape.maria03@gmail.com>
2025-07-02 19:56:15 +0300

    FUNCTIONAL DSTWR

--- src/main.c
+++ src/main.c
@@ -28,8 +28,8 @@ int main(void)
     bip_init();
     bip_config();
 
-    uint64_t T1 = 0, T4 = 0, T6 = 0;
-    uint64_t T2 = 0, T3 = 0, T5 = 0, tof_dtu;
+    static uint64_t T1 = 0, T4 = 0, T6 = 0;
+    static uint64_t T2 = 0, T3 = 0, T5 = 0;
     uint8_t Msg_id = 0;
     double distance;
     int ret;
@@ -44,10 +44,8 @@ int main(void)
 
             dw1000_write_u32(SYS_STATUS, 0xFFFFFFFF);
 
-            Msg_id++;
+            Msg_id += 1000;
 
-            dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
-            dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
             T1 = send_poll1_message(Dev_id, anchor_id, Msg_id);
             do
             {
@@ -55,6 +53,8 @@ int main(void)
 
                 if (ret == FAILURE)
                 {
+                    LOG_ERR_IF_ENABLED("Something went wrong. ABandoning this ranging cyle.");
+
                     dw1000_write_u32(SYS_STATUS, SYS_STATUS_ALL_RX_ERR | SYS_STATUS_RX_OK);
                     rx_soft_reset();
 
@@ -64,12 +64,9 @@ int main(void)
                     T5 = 0;
                     T6 = 0;
 
-                    Msg_id++;
+                    Msg_id += 1000;
 
                     k_msleep(500);
-
-                    dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
-                    dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
 
                     T1 = (uint32_t)send_poll1_message(Dev_id, anchor_id, Msg_id);
                 }
@@ -79,11 +76,14 @@ int main(void)
             clockOffsetRatio = read_carrier_integrator() *
                                (FREQ_OFFSET_MULTIPLIER * HERTZ_TO_PPM_MULTIPLIER_CHAN_2 / 1.0e6);
 
-            // LOG_INF_IF_ENABLED("For ranging cycle nr. %0d:", Msg_id);
-            // LOG_INF_IF_ENABLED("T1 = %0llu, T4 = %0llu, T5 = %0llu", T1, T4, T5);
-            // LOG_INF_IF_ENABLED("T2 = %0llu, T3 = %0llu, T6 = %0llu", T2, T3, T6);
+            LOG_INF_IF_ENABLED("For ranging cycle nr. %0d:", Msg_id);
+            LOG_INF_IF_ENABLED("T1 = %0llu, T4 = %0llu, T5 = %0llu", T1, T4, T5);
+            LOG_INF_IF_ENABLED("T2 = %0llu, T3 = %0llu, T6 = %0llu", T2, T3, T6);
 
-            double distance = compute_ds_twr_distance_basic(T1, T2, T3, T4, T5, T6);
+            int64_t Tround1 = T4 - T1;
+            int64_t Tround2 = T6 - T3;
+
+            distance = compute_ds_twr_distance_basic(T1, T2, T3, T4, T5, T6);
 
             LOG_WRN("Distance = %0f", distance);
 
