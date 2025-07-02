a8a00fdf33a5a4edd0e6cef8d3f6f90e068b453e
heldigrad <agape.maria03@gmail.com>
2025-07-02 16:42:33 +0300

    Kind of functional DSTWR

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 // #include "C:\Users\agape\Documents\LICENTA\dw1000_app\functions\dw1000_ranging_functions.h"
 
 double distances[NR_OF_ANCHORS] = {0};
+float clockOffsetRatio;
 
 int main(void)
 {
@@ -27,8 +28,9 @@ int main(void)
     bip_init();
     bip_config();
 
-    uint64_t T1, T2, T3, T4, T5, T6;
-    uint8_t Msg_id;
+    uint64_t T1 = 0, T4 = 0, T6 = 0;
+    uint64_t T2 = 0, T3 = 0, T5 = 0, tof_dtu;
+    uint8_t Msg_id = 0;
     double distance;
     int ret;
 
@@ -38,11 +40,14 @@ int main(void)
         {
             set_antenna_delay(anchor_id);
 
-            Msg_id = 0;
             distance = 0;
 
             dw1000_write_u32(SYS_STATUS, 0xFFFFFFFF);
 
+            Msg_id++;
+
+            dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
+            dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
             T1 = send_poll1_message(Dev_id, anchor_id, Msg_id);
             do
             {
@@ -61,25 +66,28 @@ int main(void)
 
                     Msg_id++;
 
-                    k_msleep(10000);
-                    T1 = send_poll1_message(Dev_id, anchor_id, Msg_id);
+                    k_msleep(500);
+
+                    dw1000_subwrite_u40(RX_TIME, 0x00, 0x00);
+                    dw1000_subwrite_u40(TX_TIME, 0x00, 0x00);
+
+                    T1 = (uint32_t)send_poll1_message(Dev_id, anchor_id, Msg_id);
                 }
 
             } while (T2 == 0 || T3 == 0 || T6 == 0);
 
-            LOG_INF_IF_ENABLED("For ranging attempt nr. %0d:", Msg_id);
-            LOG_INF_IF_ENABLED("T1 = %0llX, T4 = %0llX, T5 = %0llX", T1, T4, T5);
-            LOG_INF_IF_ENABLED("T2 = %0llX, T3 = %0llX, T6 = %0llX", T2, T3, T6);
+            clockOffsetRatio = read_carrier_integrator() *
+                               (FREQ_OFFSET_MULTIPLIER * HERTZ_TO_PPM_MULTIPLIER_CHAN_2 / 1.0e6);
 
-            uint64_t Treply1 = T3 - T2;
-            uint64_t Treply2 = T5 - T4;
-            uint64_t Tround1 = T4 - T1;
-            uint64_t Tround2 = T6 - T3;
+            // LOG_INF_IF_ENABLED("For ranging cycle nr. %0d:", Msg_id);
+            // LOG_INF_IF_ENABLED("T1 = %0llu, T4 = %0llu, T5 = %0llu", T1, T4, T5);
+            // LOG_INF_IF_ENABLED("T2 = %0llu, T3 = %0llu, T6 = %0llu", T2, T3, T6);
 
-            LOG_INF_IF_ENABLED("Treply1 = %0llX, Treply2 = %0llX, Tround1 = %0llX, Tround2 = %0llX", Treply1, Treply2, Tround1, Tround2);
+            double distance = compute_ds_twr_distance_basic(T1, T2, T3, T4, T5, T6);
 
-            k_msleep(10000);
+            LOG_WRN("Distance = %0f", distance);
+
+            k_msleep(500);
         }
-        LOG_INF_IF_ENABLED("All distances acquired!\n\r");
     }
 }
